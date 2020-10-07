9d387237f8d796f6a15427093bd8fc86fb78cb0b
Ed Summers <ehs@pobox.com>
2020-10-07 16:09:04 -0400

    use bleach correctly

--- toapi.py
+++ toapi.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import re
 import csv
 import dotenv
 import bleach
@@ -13,13 +14,18 @@ base = os.environ['AIRTABLE_BASE']
 key = os.environ['AIRTABLE_KEY']
 table = airtable.Airtable(base, 'Bio Import', key)
 
+rows = []
 for row in csv.DictReader(open('events-speaker-html.csv')):
     html = row['speaker_bio']
     html = html.replace('_x000D_', ' ')
-    html = bleach.clean(html, tags=['b', 'em', 'a', 'strong', 'i'])
-    bio = pypandoc.convert_text(html, 'md', format='html')
+    html = html.replace('\n', ' ')
+    html = re.sub('\xa0', ' ', html)
+    html = re.sub('  +', ' ', html)
+    html = bleach.clean(html, tags=['b', 'em', 'a', 'strong', 'i'], strip=True)
 
-    pypandoc.convert_text(row['speaker_bio'], 'md', format='html')
+    bio = pypandoc.convert_text(html, 'md', format='html')
+    bio = bio.replace('\n', ' ')
+
     table.insert({
         'full name': row['speaker_name'],
         'first name': row['first name'],
